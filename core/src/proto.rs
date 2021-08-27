e9f5198d44e5c636c8d87249a77b8082e5373af4
Georg Brandl <georg@python.org>
2021-08-27 14:30:05 +0200

    update to time 0.3

--- core/src/proto.rs
+++ core/src/proto.rs
@@ -45,7 +45,7 @@ lazy_static! {
     "#).expect("valid regex");
 }
 
-pub const IDENT_REPLY: &str = "SINE2020&ISSE,SECoP,V2018-11-07,v1.0\\beta";
+pub const IDENT_REPLY: &str = "SINE2020&ISSE,SECoP,V2019-09-16,v1.0";
 
 /// Enum that represents any message that can be sent over the network in the
 /// protocol, and some others that are only used internally.
